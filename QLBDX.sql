1eda864b71856609c3396c1ef235a821c3d5aff6
DieuHa <hale.081296@gmail.com>
2018-04-13 01:10:33 +0700

    CSDL

---
+++ QLBDX.sql
@@ -0,0 +1,45 @@
+create database QLBDX
+use QLBDX
+
+create table Loaixe
+(
+idloaixe char(10) primary key,
+tenloaixe varchar(45),
+)
+create table Loaive 
+(
+idloaive char(10) primary key,
+tenloaive varchar(45),
+)
+create table Nhanxe
+(
+idnhanxe char(10) primary key,
+Ngayvao date, 
+Giovao datetime, 
+biensoxe char(10) references Xe(biensoxe),
+)
+create table Ve
+(
+idve char(10) primary key,
+tenve varchar(45),
+manhanxe char(10) references Nhanxe(idnhanxe),
+idloaive char(10) references Loaive(idloaive),
+)
+create table Traxe
+(
+idtraxe char(10) primary key,
+Ngayra date,
+Giora datetime,
+idve char(10) references Ve(idve),
+)
+
+create table Xe
+(
+biensoxe char(10) primary key, 
+tenxe varchar(45),
+idloaixe char(10) references Loaixe(idloaixe),
+idtraxe char(10) references Traxe(idtraxe),
+)
+
+
+
